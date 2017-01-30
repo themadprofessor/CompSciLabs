fee53ef4805ec429cdf83d12cb54a897cbca803e
unknown <2258082R@BO715-5-05.ad.dcs.gla.ac.uk>
2017-01-30 17:22:11 +0000

    Fix week3

--- seim2/week3lab/robust.py
+++ seim2/week3lab/robust.py
@@ -1,30 +1,32 @@
 from birthdays import *
+import os
+import sys
 
-print "Imported"
+print "egg"; print "more eggs"
+
 def is_int(num):
     if num[0] in ('-', '+'):
         return num[1:].isdigit()
     return num.isdigit()
 
-print "Is int"
 def exit():
-    import sys
     sys.exit()
 
-print "Exit"
 def read():
     global BIRTHDAY_BOOK
     path = raw_input("File> ")
 
     if not os.path.exists(path):
-    	print path, "does not exist!"
+        print path, "does not exist!"
     elif not os.path.isfile(path):
         print path, "is not a file!"
-	else:
-		try:
-			BIRTHDAY_BOOK = load_book(raw_input("File> "))
-		except IOError:
-			print "Could not open", path, "!"
+    else:
+        try:
+            BIRTHDAY_BOOK = load_book(path)
+        except IOError:
+            print "Could not open", path, "!"
+        except IndexError:
+            print "Invlaid syntax!"
 
 def add():
     global BIRTHDAY_BOOK
@@ -40,58 +42,58 @@ def add():
         else: 
             day = int(day)
             month_num = MONTHS[month]
-			if (month_num == 2 and day >= 29) or (month_num % 2 == 0 and day >= 30) or month_num >= 31:
-				print "Please specify a valid day!"
-			else:
-				BIRTHDAY_BOOK[name] = {"month":month, "day":day}
+            if (month_num == 2 and day >= 29) or (month_num % 2 == 0 and day >= 30) or month_num >= 31:
+                print "Please specify a valid day!"
+            else:
+                BIRTHDAY_BOOK[name] = {"month":month, "day":day}
 
 def showall():
     for name in BIRTHDAY_BOOK:
         print_bday_person(BIRTHDAY_BOOK, name)
 
 def show():
-    if len(BIRTHDAYS_BOOK == 0):
+    if len(BIRTHDAY_BOOK) == 0:
         print "No known birthdays!"
     else:
-    	name = raw_input("Name> ")
-		if name not in BIRTHDAY_BOOK:
-			print name, "is not a known name!"
-		else:
-    	    print_bday_person(BIRTHDAY_BOOK, name)
+        name = raw_input("Name> ")
+        if name not in BIRTHDAY_BOOK:
+            print name, "is not a known name!"
+        else:
+            print_bday_person(BIRTHDAY_BOOK, name)
 
 def showmonth():
-    if len(BIRTHDAYS_BOOK == 0):
+    if len(BIRTHDAY_BOOK) == 0:
         print "No known birthdays!"
     else:
-		month = raw_input("Month> ")
-		if month not in MONTHS:
-			print month, "is not a valid month! Please use:", MONTHS.keys()
-		else:
-			print_bday_month(BIRTHDAY_BOOK, month)
+        month = raw_input("Month> ")
+        if month not in MONTHS:
+            print month, "is not a valid month! Please use:", MONTHS.keys()
+        else:
+            print_bday_month(BIRTHDAY_BOOK, month)
 
 def showweek():
-    if len(BIRTHDAYS_BOOK == 0):
+    if len(BIRTHDAY_BOOK) == 0:
         print "No known birthdays!"
     else:
-    	month = raw_input("Month> ")
-		if month not in MONTHS:
-			print month, "is not a valid month! Please use:", MONTHS.keys()
-		else:
+        month = raw_input("Month> ")
+        if month not in MONTHS:
+            print month, "is not a valid month! Please use:", MONTHS.keys()
+        else:
             month_num = MONTHS[month]
-			if (month_num == 2 and day > 29) or (month_num % 2 == 0 and day > 30) or month_num > 31:
-				print "Please specify a valid day!"
-			else:
-    	        print_bday_week(BIRTHDAY_BOOK, month, day)
+            if (month_num == 2 and day > 29) or (month_num % 2 == 0 and day > 30) or month_num > 31:
+                print "Please specify a valid day!"
+            else:
+                print_bday_week(BIRTHDAY_BOOK, month, day)
 
 def rm():
-    if len(BIRTHBOOK_BOOK == 0):
+    if len(BIRTHBOOK_BOOK) == 0:
         print "No known birthdays!"
     else:
-		name = raw_input("Name> ")
-		if name not in BIRTHBOOK_BOOK:
-			print name, "is not a known name!"
-		else:
-    	    del BIRTHDAY_BOOK[raw_input("Name> ")]
+        name = raw_input("Name> ")
+        if name not in BIRTHBOOK_BOOK:
+            print name, "is not a known name!"
+        else:
+            del BIRTHDAY_BOOK[raw_input("Name> ")]
 
 BIRTHDAY_BOOK = {}
 CMDS = {"read": read,
