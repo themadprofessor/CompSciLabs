356e321f9353a7ba0cef6f9b6d6ec05fb66fa12f
Stuart <stu@reilly-family.co.uk>
2017-11-03 15:38:34 +0000

    Add lvl2 week7

---
+++ lvl2/week7/Submission7/src/game/GameMain.java
@@ -0,0 +1,64 @@
+package game;
+
+import java.util.Scanner;
+
+/**
+ * Main method for Rock-Paper-Scissors-Lizard-Spock game.
+ * 
+ * JP2 Laboratory 7 2017.
+ * 
+ * @author mefoster
+ */
+public class GameMain {
+
+	/**
+	 * Prompts the user for the tournament parameters and then runs
+	 * a tournament.
+	 */
+	public static void main(String[] args) {
+		// Read everything from standard input
+		Scanner stdin = new Scanner(System.in);
+
+		// First player is always a computer
+		GamePlayer player1 = new ComputerPlayer("Computer");
+		
+		// Second player may be a computer or a human
+		GamePlayer player2;
+		System.out.println("Enter name of human player, or empty string for two computer players");
+		String name = stdin.nextLine();
+		if (name.length() == 0) {
+			System.out.println("Using two computer players");
+			player2 = new ComputerPlayer("Computer2");
+		} else {
+			player2 = new HumanPlayer(name, stdin);
+		}
+
+		// Get the number of games required to win the tournament -- and be sure
+		// it is a positive integer
+		int numGames = -1;
+		while (numGames <= 0) {
+			System.out.println("Enter number of games to win: ");
+			try {
+				numGames = stdin.nextInt();
+			} catch (NumberFormatException ex) {
+				System.out.println("Invalid input!");
+			}
+			if (numGames <= 0) {
+				System.out.println("Invalid input!");
+			}
+		}
+		
+		// Run the tournament with the given parameters
+		GamePlayer winner = playTournament(player1, player2, numGames);
+
+		System.out.println("------------------");
+		System.out.println("Overall winner is: " + winner.getName());
+
+		stdin.close();
+	}
+
+	private static GamePlayer playTournament(GamePlayer player1, GamePlayer player2, int numGames) {
+		// TODO put your code here (and remove this comment!!!)
+		return null;
+	}
+}
