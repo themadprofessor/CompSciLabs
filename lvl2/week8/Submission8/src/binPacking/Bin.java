df5973764f4f4081024f7393b0ef70a7e4eb6882
Stuart Reilly <stu@reilly-family.co.uk>
2017-11-13 15:26:39 +0000

    Add Javadoc

--- lvl2/week8/Submission8/src/binPacking/Bin.java
+++ lvl2/week8/Submission8/src/binPacking/Bin.java
@@ -3,17 +3,29 @@ package binPacking;
 import java.util.Arrays;
 import java.util.Objects;
 
+/**
+ * A bin for storing up to finite amount of content.
+ */
 public class Bin {
     private final int capacity;
     private int[] contents;
     private int currentCapacity;
     private int index;
 
+    /**
+     * Creates a new bin with the given capacity.
+     * @param capacity capacity of bin
+     */
     public Bin(final int capacity) {
         this.capacity = capacity;
         contents = new int[capacity];
     }
 
+    /**
+     * Stores the given value in the bin.
+     * @param weight value to be stored
+     * @throws IllegalArgumentException Thrown if the given weight would exceed this Bin's capacity if added
+     */
     public void store(int weight) throws IllegalArgumentException {
         if (currentCapacity + weight > capacity) {
             throw new IllegalArgumentException("weight is too large for this bin");
@@ -23,10 +35,17 @@ public class Bin {
         index++;
     }
 
+    /**
+     * Returns the available space in this bin.
+     * @return space in this bin
+     */
     public int getSpace() {
         return capacity - currentCapacity;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -36,6 +55,9 @@ public class Bin {
                 Arrays.equals(contents, bin.contents);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int hashCode() {
         return Objects.hash(capacity, Arrays.hashCode(contents));
--- lvl2/week8/Submission8/src/binPacking/PackingStrategy.java
+++ lvl2/week8/Submission8/src/binPacking/PackingStrategy.java
@@ -4,7 +4,16 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.stream.Collectors;
 
+/**
+ * A class containing static methods which will store the given weights into a few bins as possible.
+ */
 public class PackingStrategy {
+    /**
+     * Stores the given weights in as few Bins of given capacity as possible using the Best Fit Decreasing algorithm.
+     * @param weights weights to be stored
+     * @param capacity capacity of bins
+     * @return list of bins storing the given weights
+     */
     public static List<Bin> packBestFit(List<Integer> weights, int capacity) {
         List<Bin> bins = new ArrayList<>();
         weights.stream()
@@ -17,12 +26,26 @@ public class PackingStrategy {
         return bins;
     }
 
+    /**
+     * Creates a new Bin with given capacity, store the new Bin in the given list and return the new Bin.
+     * @param capacity capacity of the bin
+     * @param bins list of bins to add the new list to
+     * @return new bin
+     */
     private static Bin addNewBin(int capacity, List<Bin> bins) {
         Bin bin = new Bin(capacity);
         bins.add(bin);
         return bin;
     }
 
+    /**
+     * Stores the given weights in as few Bins of given capacity as possible using the Best Fit Decreasing algorithm by
+     * splitting the task among the given number of threads.
+     * @param allInts weights to be stored
+     * @param capacity capacity of bins
+     * @param numThreads number of threads
+     * @return list of bins storing the weights
+     */
     public static List<Bin> packBestFitParallel(List<Integer> allInts, int capacity, int numThreads) {
         ExecutorService pool = Executors.newFixedThreadPool(numThreads);
         Collection<Callable<List<Bin>>> tasks = new ArrayList<>();
@@ -46,11 +69,17 @@ public class PackingStrategy {
         }
     }
 
+    /**
+     * Waits for the given Future to finish, and return its value. If the Future throws an exception, return an empty
+     * list.
+     * @param future future to wait for
+     * @return list of bin produced by given future or an empty list if the future fails
+     */
     private static List<Bin> getResult(Future<List<Bin>> future) {
         try {
             return future.get();
         } catch (InterruptedException | ExecutionException e) {
-            return new ArrayList<>();
+            return Collections.emptyList();
         }
     }
 }
