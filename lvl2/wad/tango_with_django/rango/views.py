9d398e854b836fdd52af37053f3f954b7680b8da
Stuart <stu@reilly-family.co.uk>
2018-01-16 19:35:03 +0000

    Change /rango/ to use HTML template

--- lvl2/wad/tango_with_django/rango/views.py
+++ lvl2/wad/tango_with_django/rango/views.py
@@ -1,8 +1,10 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says hey there partner! </br> <a href='about'>About</a>")
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return HttpResponse("Rango says here is the about page")
