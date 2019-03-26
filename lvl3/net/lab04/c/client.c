720a064d793bddd2c64ea43d4e8019b583e6925c
stuart <stu@reilly-family.co.uk>
2019-03-26 23:09:27 +0000

    Comment net lab4

--- lvl3/net/lab04/c/client.c
+++ lvl3/net/lab04/c/client.c
@@ -5,8 +5,10 @@
 #include <netdb.h>
 #include <stdlib.h>
 
+// Some macro magic to convert a macro's value into a string
 #define STR_ARG(s) __STR_ARG(s)
 #define __STR_ARG(s) #s
+
 #define PORT 5006
 #define PORT_STR STR_ARG(PORT)
 #define eprintf(...) fprintf(stderr, __VA_ARGS__)
@@ -16,7 +18,10 @@ int address_to_socket(char* address, void* dest) {
     int fd = -1;
     struct sockaddr_in* in_addr = dest;
 
+    // Attempt to convert the given address into an IPv4 address
     result = inet_pton(AF_INET, address, &(in_addr->sin_addr));
+
+    // If it failures, run a DNS lookup on the address, otherwise use the address
     if (result != 1) {
         struct addrinfo hints;
         struct addrinfo* addr_list;
@@ -24,30 +29,44 @@ int address_to_socket(char* address, void* dest) {
 
         eprintf("not given address, resolving address\n");
 
+        // Initialise DNS hints to only allow IPv4 datagram addresses
         memset(&hints, 0, sizeof(hints));
         hints.ai_family = AF_INET;
         hints.ai_socktype = SOCK_DGRAM;
 
+        // Run DNS lookup and handle failure
         result = getaddrinfo(address, PORT_STR, &hints, &addr_list);
         if (result != 0) {
             eprintf("failed to resolve address: %s", gai_strerror(result));
             return -1;
         }
 
+        // Iterate over the list of addresses and attempt to open a socket to each address, breaking upon first success
         for (curr_addr = addr_list; curr_addr != NULL; curr_addr = curr_addr->ai_next) {
+            // Attempt to open socket, run next iteration if it fails
             fd = socket(curr_addr->ai_family, curr_addr->ai_socktype, curr_addr->ai_protocol);
             if (fd == -1) {
                 continue;
             }
+
+            // Break from the loop as the socket successfully opened
             eprintf("found address\n");
             break;
         }
 
+        // Free the address list
         freeaddrinfo(addr_list);
 
-        return fd == -1 ? -1 : fd;
+        // If no addresses successfully opened a socket (fd == -1), return -1, otherwise return the socket
+        if (fd == -1) {
+            return -1;
+        }
 
+        // Update dest point with new address and return the socket
+        memcpy(dest, curr_addr->ai_addr, curr_addr->ai_addrlen);
+        return fd;
     } else {
+        // Since the address is a valid address, open a socket
         eprintf("given address, opening socket\n");
         fd = socket(AF_INET, SOCK_DGRAM, 0);
         if (fd == -1) {
@@ -60,23 +79,29 @@ int address_to_socket(char* address, void* dest) {
 
 int main(int argc, char** argv) {
     int fd;
-    struct sockaddr_in address;
-    char msg[] = "Hello World";
+    struct sockaddr_in address; // Address to send data to
+    char msg[] = "Hello World"; // Data to be sent
 
+    // Ensure the address is given as an argument
     if (argc < 2) {
         eprintf("invalid arg count\n");
         return EXIT_FAILURE;
     }
 
+    // Initialise address to be IPv4 and send to port number PORT
+    memset(&address, 0, sizeof(address));
     address.sin_family = AF_INET;
     address.sin_port = htons(PORT);
 
+    // Open a socket
     fd = address_to_socket(argv[1], &address);
 
+    // Handle failure
     if (fd == -1) {
         return EXIT_FAILURE;
     }
 
+    // Send data and handle failure
     if (sendto(fd, msg, sizeof(msg), 0, (struct sockaddr*) &address, sizeof(address)) < 0) {
         perror("failed to send data");
         return EXIT_FAILURE;
--- lvl3/net/lab04/c/server.c
+++ lvl3/net/lab04/c/server.c
@@ -10,32 +10,37 @@
 #define BUFFER_LEN 1024
 
 int main() {
-    struct sockaddr_in bind_addr;
-    struct sockaddr recv_addr;
-    int fd;
-    ssize_t received;
-    char buffer[BUFFER_LEN] = {0};
+    struct sockaddr_in bind_addr; // Address the socket will be bound to
+    struct sockaddr recv_addr; // The address of the sender of the data
+    int fd; //The socker
+    ssize_t received; // Amount of data received
+    char buffer[BUFFER_LEN] = {0}; // Buffer to hold the data sent, {0} zeros the buffer
     socklen_t recv_addr_len = sizeof(recv_addr);
 
-    fd = socket(AF_INET, SOCK_DGRAM, 0);
+    fd = socket(AF_INET, SOCK_DGRAM, 0); // Attempt to open the socket
 
+    // Handle failure
     if (fd == -1) {
         perror("failed to open socket");
         return EXIT_FAILURE;
     }
     eprintf("opened socket\n");
 
+    // Initialise binding address to be IPv4, at port number PORT and accept data from any sender
     memset(&bind_addr, 0, sizeof(bind_addr));
     bind_addr.sin_family = AF_INET;
     bind_addr.sin_port = htons(PORT);
     bind_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
+    // Bind socket and handle failure
     if (bind(fd, (const struct sockaddr *) &bind_addr, sizeof(bind_addr)) != 0) {
         perror("failed to bind socket");
         return EXIT_FAILURE;
     }
     eprintf("bound socket\n");
 
+    // Block until data is sent, then store it in the buffer and handle failure
+    // The size of the buffer is reduced by 1 to make sure the buffer is always zero-terminated
     received = recvfrom(fd, buffer, sizeof(buffer)-1, 0, &recv_addr, &recv_addr_len);
     if (received < 0) {
         perror("failed to receive data");
@@ -43,8 +48,10 @@ int main() {
     }
     eprintf("received %ld bytes\n", received);
 
+    // Print the data
     printf("%s\n", buffer);
 
+    // Cleanup
     if (close(fd) == -1) {
         perror("failed to close socket");
         return EXIT_FAILURE;
