cd6506ffb678c343853453361288a9684fc91a2d
Stuart Reilly <stu@reilly-family.co.uk>
2019-03-26 17:32:14 +0000

    Finish rust impl

--- lvl3/net/lab04/rust/client/src/main.rs
+++ lvl3/net/lab04/rust/client/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(process_exitcode_placeholder)]
+
+use std::env;
+use std::net::{Ipv4Addr, SocketAddrV4, UdpSocket};
+use std::process::ExitCode;
+
+fn main() -> Result<(), ExitCode> {
+    let address: SocketAddrV4 = match env::args().nth(1) {
+        Some(port) => port.parse().map_err(|err| {
+            eprintln!("invalid address: {}", err);
+            ExitCode::FAILURE
+        }),
+        None => {
+            eprintln!("missing address");
+            Err(ExitCode::FAILURE)
+        }
+    }?;
+
+    let socket = UdpSocket::bind(("127.0.0.1".parse::<Ipv4Addr>().unwrap(), 10000))
+        .map_err(|err| {
+            eprintln!("failed to bind socket: {}", err);
+            ExitCode::FAILURE
+        })?;
+
+    socket
+        .send_to("Hello World".as_ref(), address)
+        .map_err(|err| {
+            eprintln!("failed to send data: {}", err);
+            ExitCode::FAILURE
+        })
+        .map(|_| ())
 }
--- lvl3/net/lab04/rust/server/src/main.rs
+++ lvl3/net/lab04/rust/server/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(process_exitcode_placeholder)]
+
+use std::env;
+use std::net::{Ipv4Addr, UdpSocket};
+use std::process::ExitCode;
+
+const BUFFER_SIZE: usize = 1024;
+
+fn main() -> Result<(), ExitCode> {
+    let port = match env::args().nth(1) {
+        Some(port) => port.parse().map_err(|err| {
+            eprintln!("invalid port number: {}", err);
+            ExitCode::FAILURE
+        }),
+        None => {
+            eprintln!("missing port number");
+            Err(ExitCode::FAILURE)
+        }
+    }?;
+
+    let socket =
+        UdpSocket::bind(("127.0.0.1".parse::<Ipv4Addr>().unwrap(), port)).map_err(|err| {
+            eprintln!("failed to open socket: {}", err);
+            ExitCode::FAILURE
+        })?;
+
+    let mut buffer = [0; BUFFER_SIZE];
+    let (count, addr) = socket.recv_from(&mut buffer).map_err(|err| {
+        eprintln!("failed to receive datagram: {}", err);
+        ExitCode::FAILURE
+    })?;
+
+    println!("{}", String::from_utf8_lossy(&buffer[..count]));
+    println!("{}", addr);
+
+    Ok(())
 }
